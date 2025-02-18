a3f4a8d58b47430e4edfcf23bbeef7d36b199476
Mkps <alex.loubiere@gmail.com>
2025-02-18 13:12:07 +0100

    adding privilege escalation with pkexec

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ static void activate (GtkApplication *app, gpointer user_data)
   (void)user_data;
   /* Construct a GtkBuilder instance and load our UI description */
   GtkBuilder *builder = gtk_builder_new ();
-  gtk_builder_add_from_file (builder, "src/builder.ui", NULL);
+  gtk_builder_add_from_file (builder, "/home/alx/Code/Powermanager/src/builder.ui", NULL);
 
   /* Connect signal handlers to the constructed widgets. */
   GObject *window = gtk_builder_get_object (builder, "window");
@@ -33,12 +33,26 @@ static void activate (GtkApplication *app, gpointer user_data)
   g_object_unref (builder);
 }
 
-int main (int   argc, char *argv[])
+int main (int   argc, char *argv[], char **env)
 {
 #ifdef GTK_SRCDIR
   g_chdir (GTK_SRCDIR);
 #endif
-  GtkApplication *app = gtk_application_new ("org.gtk.PowerManager", G_APPLICATION_DEFAULT_FLAGS);
+  if (geteuid() != 0) {  // If not root, relaunch with pkexec
+    char *exec_args[256];
+    bzero(exec_args, sizeof(exec_args));
+    exec_args[0] = "pkexec";
+    exec_args[1] = "env";
+    int i = 0;
+    for (; env[i] && i < 250; ++i) {
+      exec_args[i + 2] = env[i];
+    }
+    exec_args[i + 2] =  "/home/alx/Code/Powermanager/PowerManager";
+    execvp("pkexec", exec_args);
+    perror("Failed to execute pkexec");
+    return EXIT_FAILURE;
+  }
+  GtkApplication *app = gtk_application_new ("com.mkps.powermanager", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
 
   int status = g_application_run (G_APPLICATION (app), argc, argv);
